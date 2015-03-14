b7f44c77e2cf66ed94eccac99c9c19c6b988ae84
JMLjml <johnlasheski@gmail.com>
2015-03-14 17:11:23 -0400

    added methods to SortInterace

--- Radix_Sort_Benchmark_Project/src/RadixSort.java
+++ Radix_Sort_Benchmark_Project/src/RadixSort.java
@@ -8,4 +8,28 @@
  */
 public class RadixSort implements SortInterface {
 
+  @Override
+  public int[] recursiveSort(int[] list) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int[] iterativeSort(int[] list) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getCount() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public long getTime() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
 }
--- Radix_Sort_Benchmark_Project/src/SortInterface.java
+++ Radix_Sort_Benchmark_Project/src/SortInterface.java
@@ -7,5 +7,13 @@
  *
  */
 public interface SortInterface {
+  
+  public int[] recursiveSort(int[] list);
+  
+  public int[] iterativeSort(int[] list);
+  
+  public int getCount();
+  
+  public long getTime();
 
 }
