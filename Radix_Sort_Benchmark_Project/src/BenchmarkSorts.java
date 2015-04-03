761222940166106085f7894ffc3faedaf05a5d64
JMLjml <johnlasheski@gmail.com>
2015-04-03 17:46:58 -0400

    Created testData array. Also implemented loop to test all sets of data.

--- Radix_Sort_Benchmark_Project/src/BenchmarkSorts.java
+++ Radix_Sort_Benchmark_Project/src/BenchmarkSorts.java
@@ -12,45 +12,107 @@ import java.util.Random;
  */
 public class BenchmarkSorts {
   private int[] sizes;
+  private int[][][] testData;
   
   public BenchmarkSorts(int[] sizes) {
-    this.sizes = sizes;    
+    this.sizes = sizes;
+    createTestData();
   }
   
-  public void runSorts() {
+  private void createTestData() {
     Random randomGenerator = new Random();
     
+    this.testData = new int[10][50][];
     
-    int[] list = new int[50];
+    // Use the sizes array to get the length for each set of test data
+    for(int i = 0; i < 10; i++) {
+      for(int j = 0; j < 50; j++) {
+        testData[i][j] = new int[this.sizes[i]];
+        
+        // Populate each test Array with some random numbers
+        for(int k = 0; k < this.sizes[i]; k++) {
+          testData[i][j][k] = randomGenerator.nextInt(100000);
+        }
+        
+      }
+    }
+ 
+  }
+  
+  
+  
+  
+  
+  public void runSorts() {
     
-    for(int i = 0; i < list.length; i++) {
-      list[i] = randomGenerator.nextInt(100000);
+    System.out.print("Starting Tests");
+    
+    
+    for(int i = 0; i < this.sizes.length; i++) {
+      
+      System.out.println("\nSorting Lists of size " + this.sizes[i] + ".");
+      
+      for(int j = 0; j < 50; j++) {
+        
+        SortInterface sort = new RadixSort();
+        
+        int[] list = (int[])testData[i][j].clone();
+        
+//        for(int k = 0; k < list.length; k++) {
+//          System.out.print(list[k] + " ");
+//        }
+        
+        sort.iterativeSort(list);
+        
+      //  System.out.println("\n");
+        
+//        for(int k = 0; k < list.length; k++) {
+//          System.out.print(list[k] + " ");
+//        }
+//        
+//        System.out.println("\n");
+        
+        try {
+          verifySorted(list);
+        } catch (UnsortedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        
+        
+        
+        sort = new RadixSort();
+        
+        list = (int[])testData[i][j].clone();
+//        
+//        for(int k = 0; k < list.length; k++) {
+//          System.out.print(list[k] + " ");
+//        }
+        
+        sort.recursiveSort(list);
+        
+   //     System.out.println("\n");
+//        
+//        for(int k = 0; k < list.length; k++) {
+//          System.out.print(list[k] + " ");
+//        }
+        
+     //   System.out.println("\n");
+        
+        try {
+          verifySorted(list);
+        } catch (UnsortedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        
+        System.out.print("*");
+        
+               
+      }
     }
     
-    
-    
-    SortInterface sort = new RadixSort();
-    
-    
-    for(int i = 0; i < list.length; i++) {
-      System.out.print(list[i] + " ");
-    }
-    
-    System.out.println("\n");
-    
-    
-    sort.recursiveSort(list);
-    
-    for(int i = 0; i < list.length; i++) {
-      System.out.print(list[i] + " ");
-    }
-    
-    try {
-      verifySorted(list);
-    } catch (UnsortedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    System.out.println("\nTests Complete.");
     
   }
   
--- Radix_Sort_Benchmark_Project/src/SortMain.java
+++ Radix_Sort_Benchmark_Project/src/SortMain.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 /**
  * 
  */
@@ -11,18 +8,11 @@ import java.util.Random;
  */
 public class SortMain {
 
-  /**
-   * @param args
-   */
   public static void main(String[] args) {
-    System.out.println("Hello John. Would you like to play a game of thermo-nuclear war?");
-    
-    int[] sizes = {100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 250,000, 500000};
-    
+    int[] sizes = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
     BenchmarkSorts benchmark = new BenchmarkSorts(sizes);
     
     benchmark.runSorts();
     benchmark.displayReport();
-    
   }
 }
