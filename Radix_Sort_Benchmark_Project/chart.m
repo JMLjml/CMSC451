07b79f94979a11351215f3c7c7cad31af4e5b68c
JMLjml <johnlasheski@gmail.com>
2015-04-29 19:06:07 -0400

    Iterative Time Charts complete

--- Radix_Sort_Benchmark_Project/chart.m
+++ Radix_Sort_Benchmark_Project/chart.m
@@ -15,23 +15,263 @@ iTime = dlmread('iTimeFile.dat');
 
 % N should be 50, create a vector n to plot against
 N = length(iTime);
-n = [1:1:N]
+n = [1:1:N];
 
 
-% figure 1 will be the iterative time data for each test case, 10 total cases
+% figures 1 - 10 will be the iterative time data for each test case, 10 total cases
 figure(1)
 hold on;
 
 % plot the time data
-row = iTime(1,:)
+row = iTime(1,:);
 plot(n, row / 100000, 'color', 'm');
-xlabel('Test Case (n)', 'fontsize', 10);
-ylabel('Time (ms)', 'fontsize', 10);
-title('N = 128');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 128', 'fontsize', 14);
 
 % calculate the mean and std dev
-s = std(row) / 100000
-m = sum(row) / N / 100000
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+figure(2)
+hold on;
+
+% plot the time data
+row = iTime(2,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 256', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+figure(3)
+hold on;
+
+% plot the time data
+row = iTime(3,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 512', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(4)
+hold on;
+
+% plot the time data
+row = iTime(4,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 1,024', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+figure(5)
+hold on;
+
+% plot the time data
+row = iTime(5,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 2,048', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(6)
+hold on;
+
+% plot the time data
+row = iTime(6,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 4,096', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(7)
+hold on;
+
+% plot the time data
+row = iTime(7,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 8,192', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(8)
+hold on;
+
+% plot the time data
+row = iTime(8,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 16,384', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(9)
+hold on;
+
+% plot the time data
+row = iTime(9,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 32,768', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
+
+% plot the mean
+plot(25, m, 'marker', 'v', 'markersize', 10);
+
+% plot the mean +- 1 std dev
+plot(n, m + s, 'o', 'color', 'g');
+plot(n, m - s, 'o', 'color', 'g');
+
+legend('Time (ms)', 'mean', 'Std Dev');
+hold off;
+
+
+
+
+figure(10)
+hold on;
+
+% plot the time data
+row = iTime(10,:);
+plot(n, row / 100000, 'color', 'm');
+xlabel('Test Case (n)', 'fontsize', 14);
+ylabel('Time (ms)', 'fontsize', 14);
+title('N = 65,536', 'fontsize', 14);
+
+% calculate the mean and std dev
+s = std(row) / 100000;
+m = sum(row) / N / 100000;
 
 % plot the mean
 plot(25, m, 'marker', 'v', 'markersize', 10);
